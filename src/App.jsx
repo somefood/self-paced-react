08e7978dac9d13d555074dfc60bfcbc98ee30703
Seokju Hong <iamseokjuhong@gmail.com>
2024-08-25 16:49:02 +0900

    2단계 - Props와 State 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import "./App.css";
 import Header from "./components/Header.jsx";
 import CategoryFilter from "./components/CategoryFilter.jsx";
-import RestaurantDetailModal from "./components/RestaurantDetailModal.jsx";
-import AddRestaurantModal from "./components/AddRestaurantModal.jsx";
+// import RestaurantDetailModal from "./components/RestaurantDetailModal.jsx";
+// import AddRestaurantModal from "./components/AddRestaurantModal.jsx";
 import RestaurantList from "./components/RestaurantList.jsx";
+import {useState} from "react";
+import {restaurants} from "/db.json"
 
 function App() {
+  const ALL = '전체';
+  const [category, setCategory] = useState(ALL);
+  const filteredRestaurants = category === ALL
+    ? restaurants
+    : restaurants.filter(restaurant => restaurant.category === category);
+
   return (
     <>
-      <Header />
+      <Header/>
       <main>
-        <CategoryFilter />
-        <RestaurantList />
+        <CategoryFilter category={category} onChangeCategory={setCategory}/>
+        <RestaurantList restaurants={filteredRestaurants}/>
       </main>
       <aside>
-        <RestaurantDetailModal />
-        <AddRestaurantModal />
+        {/*<RestaurantDetailModal />*/}
+        {/*<AddRestaurantModal />*/}
       </aside>
     </>
   );
--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -1,10 +1,14 @@
 import './CategoryFilter.css';
 
-export default function CategoryFilter() {
+export default function CategoryFilter({category, onChangeCategory}) {
+  const handleCategoryChange = (event) => {
+    onChangeCategory(event.target.value);
+  }
+
   return (
     <>
       <section className="restaurant-filter-container">
-        <select name="category" id="category-filter" className="restaurant-filter" aria-label="음식점 카테고리 필터">
+        <select name="category" id="category-filter" className="restaurant-filter" aria-label="음식점 카테고리 필터" value={category} onChange={handleCategoryChange}>
           <option value="전체">전체</option>
           <option value="한식">한식</option>
           <option value="중식">중식</option>
